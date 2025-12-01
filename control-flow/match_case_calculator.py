df1be3c18378c7088be667ba80e7ee7e88b05264
osgoodboateng-glitch <osgoodboateng2006@gmail.com>
2025-12-01 10:10:18 +0000

    Add match-case based calculator functionality
    Implement a basic calculator using match-case for operations.

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,17 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+operation = input("Choose the operation(+, -, *, /): ")
+match operation:
+    case "+":
+        print(f"The result is {num1+num2}.")
+    case "-":
+        print(f"The result is {num1-num2}.")
+    case "*":
+        print(f"The result is {num1*num2}.")
+    case "divide":
+            try:
+                print(f"The result is {num1/num2}.")
+            except ZeroDivisionError:
+                print("Cannot divide by zero.")
+    case _:
+            print("Unknown operation")
