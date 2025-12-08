c05882f9da72ac0ddc47e602f3236f5f5ee5adc1
osgoodboateng-glitch <osgoodboateng2006@gmail.com>
2025-12-08 01:38:04 +0000

    Add bank_account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,25 @@
+class BankAccount:
+    def __init__(self, account_balance=0):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+            return True
+        else:
+            print("Deposit amount must be positive")
+            return False
+
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            print("Error: Insufficient funds")
+            return False
+        elif amount <= 0:
+            print("Withdrawal amount must be positive")
+            return False
+        else:
+            self.account_balance -= amount
+            return True
+
+    def display_balance(self):
+        return self.account_balance
